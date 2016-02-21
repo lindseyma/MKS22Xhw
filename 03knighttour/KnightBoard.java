9f616b9466a14c0c2ae303a112f9286d24a50811
lindseyma <lma2@stuy.edu>
2016-02-21 15:41:07 -0500

    made many changes but something's still not quite right

--- 03knighttour/KnightBoard.java
+++ 03knighttour/KnightBoard.java
@@ -14,39 +14,84 @@ public class KnightBoard{
 	return solveH(1, 0, 0);
     }
 
+    int endPoint = board.length * board[0].length+1;
+    int numCols = board.length - 1;
+    int numRows = board[0].length - 1;
+
+
     public boolean solveH(int num, int col, int row){
 
-	//check if in bounds
-	if(col < 0 ||
-	   col > board.length ||
-	   row < 0 ||
-	   row > board[0].length){
+	if(num == endPoint){
+	    return true;
+
+	System.out.println(num + " " + col + " " + row);
+
+	if(addKnight(num, row, col)){
+	    num+=1;
+	    if(check(col+1, row-2)){
+		return solveH(num, col+1, row-2);
+	    }
+	    else if(check(col+2, row-1)){
+		return solveH(num, col+2, row-1);
+	    }
+	    else if(check(col+2, row+1)){
+		return solveH(num, col+2, row+1);
+	    }
+	    else if(check(col+1, row+2)){
+		return solveH(num, col+1, row+2);
+	    }
+	    else if(check(col-1, row+2)){
+		return solveH(num, col-1, row+2);
+	    }
+	    else if(check(col-2, row+1)){
+		return solveH(num, col-2, row+1);
+	    }
+	    else if(check(col-2, row-1)){
+		return solveH(num, col-2, row-1);
+	    }
+	    else{
+		return solveH(num, col-1, row-2);
+	    }
+	}//recursion
+	
+	else{
 	    return false;
 	}
 
-	if(num == board.length * board[0].length + 1){
+	return false;
+      
+	}
+    }
+   
+    public boolean check(int col, int row){
+	if(col < 0 ||
+	   col > numCols ||
+	   row < 0 ||
+	   row > numRows){
+	    return false;
+	}
+	else{
 	    return true;
 	}
-	System.out.println(num + " " + col + " " + row);
-	if(board[row][col] > 0){
+    }
+    
+    public boolean addKnight(int num, int row, int col){
+	//checks if in bounds
+	if(col < 0 ||
+	   col > numCols ||
+	   row < 0 ||
+	   row > numRows){
 	    return false;
 	}
-
-	//set the # and search for next space
+	//check if already a knight here
+	if (board[row][col] > 0){
+	    return false;
+	}
+	//put the # on the board
 	else{
 	    board[row][col] = num;
-	    num+=1;
-	    return(solveH(num, col+1, row-2)||
-		   solveH(num, col+2, row-1)||
-		   solveH(num, col+2, row+1)||
-		   solveH(num, col+1, row+2)||
-		   solveH(num, col-1, row+2)||
-		   solveH(num, col-2, row+1)||
-		   solveH(num, col-2, row-1)||
-		   solveH(num, col-1, row-2));
-	}//else w/ the recursion
-	
-	
+	    return true;
+	}
     }
 
     public void printSolution(){
@@ -65,4 +110,4 @@ public static void main(String[]args){
     b.printSolution();
 }
 
-}
+    }
