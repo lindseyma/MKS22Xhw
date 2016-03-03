ba826099d988926a16caf2ce55e3e9a8991e948d
Lindsey Ma <lma2@stuy.edu>
2016-03-03 13:05:34 -0500

    started sorts

---
+++ 06Sorts/Sorts.java
@@ -0,0 +1,96 @@
+import java.util.Random;
+
+public class Sorts{
+
+    //static int[] test=new int[]{3,2,4,5};
+
+    public static void fillRandom(int[] ary){
+	    int max=10000;
+	    Random num = new Random();
+	    for(int i=0; i<ary.length; i++){
+		ary[i]=num.nextInt(max);
+	    }
+    } 
+
+    public static void swap(int[] ary, int i, int j){
+	int temp=ary[i];
+	ary[i]=ary[j];
+	ary[j]=temp;
+    }
+
+    public static void main(String[]args){
+	/*int size=5;
+	int max = 10;
+	int[] test = new int[size];
+	Random num = new Random();
+	for(int i=0; i<size; i++){
+	    test[i]=num.nextInt(max);
+	}   
+	//insertionSort(test);
+	bubbleSort(test);*/
+    }
+
+        public static void insertionSort(int[] myData){
+	    int x; //x is current number
+	    for(int i=1; i<myData.length; i++){
+		//1 because start w/ 2nd #
+		x=myData[i];
+		int j=i-1;
+		for (j=i-1; j>=0 && x < myData[j]; j--){
+		    myData[j+1]=myData[j];
+		    //}//for j
+		    myData[j+1]=x;
+		}//for j
+	    }//for i
+	    for(int k=0; k<myData.length; k++){
+		System.out.print(myData[k]);
+	    }//for k 
+	}
+	
+    public static void selectionSort(int[] myData){
+	for (int i=0; i<myData.length; i++){
+	    int x=myData[i]; //current least value
+	    int y=-1; //spot of least value
+	    for(int j=i+1; j<myData.length; j++){
+		if (myData[j]<x){
+		x=myData[j];
+		y=j;
+		}//if
+		if(x!=myData[i] && y>-1){
+		myData[y]=myData[i];
+		myData[i]=x;
+		}//if
+	    }//for j
+	    /*for(int d=0; d<myData.length; d++){
+		System.out.print(myData[d] + ", ");
+		}//d test*/
+	}//for i
+	for (int k=0; k < myData.length; k++){
+	    System.out.print(myData[k]);
+	}//for
+    }//selection	
+	 
+
+    public static void bubbleSort(int[] myData){
+	//int x=myData.length-1;
+	for(int i=0; i<myData.length; i++){
+	    for(int j=1; j<myData.length-i; j++){
+		if(myData[j-1]>myData[j]){//swap if int is > next int
+		    int x=myData[j-1];//holds the number for swapping
+		    myData[j-1]=myData[j];
+		    myData[j]=x;
+		}//if
+	    }//for j
+	    for(int j=0; j<myData.length; j++){
+		System.out.print(myData[j]);
+	    }//printing
+	}//for i
+    }//bubble
+
+    public static void mergeSort(int[] data){
+	int[]temp;
+	for(int i=0; i<data.length/2; i++){
+	    
+    
+}//Sorts
+   
