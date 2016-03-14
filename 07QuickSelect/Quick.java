aa5f12b2190b7e2eb6a50aaea98e4f54a35889ba
lindseyma <lma2@stuy.edu>
2016-03-14 00:27:19 -0400

    this hw has caused me much pain but it is finally done

--- 07QuickSelect/Quick.java
+++ 07QuickSelect/Quick.java
@@ -8,7 +8,15 @@ import java.util.*;
 
 	public static void main(String[]args){
 	    int[] data = new int[] {1, 3, 2, 9, 7, 4, 5, 6};
-	    System.out.println(quickSelect(data, 3));
+	    //System.out.println(quickSelect(data, 3));
+	    quickSort(data);
+	    printAry(data);
+	    for(int i=0; i<data.length-1; i++){
+		if(data[i+1] >= data[i]){
+		    System.out.println("t");
+		}
+		System.out.println("f");
+	    }
 	}
 
 	public static int partitionOld(int[]data, int left, int right){
@@ -32,6 +40,32 @@ import java.util.*;
 	    return left;
 	}
 
+	public static int[] partition(int[]data, int left, int right){
+	    int[] returnAry = new int[2];
+	    int randNum = (int)(Math.random()*(right-left+1))+left;
+	    swap(data, randNum, right);
+	    for(int i=left; i<right; i++){
+		if(data[i] > data[right]){
+		    swap(data, i, left);
+		    left++;
+		}
+	    }
+	    swap(data, left, right);
+	    returnAry[0] = left;
+	    int ans=0;
+	    for(int i=left; i<data.length; i++){
+		if(data[i] == left){
+		    ans++;
+		}
+	    }
+	    returnAry[1] = ans;
+	    return returnAry;
+	}
+
+	
+	  
+
+
 	public static int quickSelectOld(int[] data, int k){
 	    return quickSelectOld(data, k, 0, data.length-1);
 	}
@@ -52,7 +86,7 @@ import java.util.*;
 		    
 		    
 	    }
-	    return "Error";
+	    return -1;
 		
 	}
 
@@ -63,15 +97,32 @@ import java.util.*;
 	}
 	
 	public static void quickSortOld(int[]data){
-	    sort(data, 0, data.length-1);
+	    quickSortOld(data, 0, data.length-1);
 	}
 
 	public static void quickSortOld(int[]data, int left, int right){
 	    if(right - left > 0){
 		int pivot = partitionOld(data, left, right);
-		QuickSortOld(data, left, pivot-1);
+		quickSortOld(data, left, pivot-1);
 		quickSortOld(data, pivot + 1, right);
 	    }
 	}
 	
+	public static void quickSort(int[]data){
+	    quickSort(data, 0, data.length-1);
+	}
+	
+	public static void quickSort(int[]data, int left, int right){
+	    if(left < right){
+		int[]ary = partition(data, left, right);
+		quickSort(data, left, ary[0]-1);
+		quickSort(data, right, ary[1]+1);
+	    }
+	}
+	
+	public static void printAry(int[]data){
+	    for(int i=0; i<data.length; i++){
+		System.out.println(data[i]);
+	    }
+	}
     }
