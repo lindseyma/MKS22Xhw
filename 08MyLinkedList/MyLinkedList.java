d9002dd5c60619a25740bb736daf75150064404c
Lindsey Ma <lma2@stuy.edu>
2016-03-15 13:07:29 -0400

    finished LNode inner class

---
+++ 08MyLinkedList/MyLinkedList.java
@@ -0,0 +1,47 @@
+public class MyLinkedList{
+    
+    LNode start;
+    int size;
+   
+    public void add(int value){
+	size++;
+	
+    }
+
+    public String toString(){
+	
+    }
+
+    public int get(int index){
+	
+    }
+
+    public class LNode{
+	
+	int value;
+	LNode next;
+	
+	//get/set value and get/set next
+	public LNode(int value){
+	    this.value = value;
+	}
+	
+	public int getValue(){
+	    return value;
+	}
+
+	public void setValue(int value){
+	    this.value = value;
+	}
+	
+	public LNode getNext(){
+	    return next;
+	}
+	
+	public void setNext(LNode next){
+	    this.next = next;
+	}
+
+    }
+
+}
